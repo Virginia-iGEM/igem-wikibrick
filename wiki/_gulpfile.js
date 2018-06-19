c5501f5611b1bfeadeb508cd18c31d27b6cf7163
Mantissa-23 <dylanculfogienis@gmail.com>
2018-06-19 17:38:00 -0400

    Deleted old gulpfile

--- wiki/_gulpfile.js
+++
@@ -1,9 +0,0 @@
-// See https://github.com/Mantissa-23/VGEM-2018/tree/master/wiki for descriptions of packages
-var environments = require('gulp-environments');
-var sass = require('gulp-sass');
-var bourbon = require('node-bourbon').includePaths;
-var neat = require('node-neat').includePaths;
-var browsersync = require('browser-sync');
-
-var relative2absolute = require('gulp/helpers/relative2absolute.js');
-var upload = require('gulp/helpers/upload.js');
