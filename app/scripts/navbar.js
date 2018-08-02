edd65141ca608708918ada682ffb0e703ef2e95c
Mantissa-23 <dylanculfogienis@gmail.com>
2018-08-02 15:15:23 -0400

    Removed example app folder now that generator-igemwiki exists

--- app/scripts/navbar.js
+++
@@ -1,9 +0,0 @@
-// Code courtesy of Jay Holtslander, https://codepen.io/j_holtslander/pen/doQggE
-
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('nav').addClass('shrink');
-  } else {
-    $('nav').removeClass('shrink');
-  }
-});
